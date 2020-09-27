39960c5fdd7d2680f80d539e12a073db122761cc
Steve Jeong <jst0930@gmail.com>
2020-09-27 19:40:56 +0900

    up to React 15. State, Key

--- src/App.js
+++ src/App.js
@@ -7,11 +7,25 @@ import Content from "./components/Content"
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interaction'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="Web" sub="World Wide Web"></Subject>
-        <TOC></TOC> 
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC> 
         <Content title="HTML" desc="HyperText Markup Language"></Content> 
       </div>
     );
