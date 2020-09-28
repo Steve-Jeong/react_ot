d2f890d545fa60aec5daa15d3506eea6eedb68fa
Steve Jeong <jst0930@gmail.com>
2020-09-28 21:33:54 +0900

    up to React 16. Event. bind(this). setState

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props) {
     super();   //super(props);와 같이 하면 warning message가 뜬다.
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,14 +20,30 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e)
+            e.preventDefault()
+            this.setState({mode:'welcome'})
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          <p>{this.state.subject.sub}</p>
+        </header>
         <TOC data={this.state.contents}></TOC> 
-        <Content title="HTML" desc="HyperText Markup Language"></Content> 
+        <Content title={_title} desc={_desc}></Content> 
       </div>
     );
   }
